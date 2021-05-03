a66b6c1bcbced3b55d6c85fa2f5bf14a8d6697de
Giancarlo Buenaflor <e51837398@student.tuwien.ac.at>
2021-05-04 01:40:36 +0200

    Create user table and data gen files

---
+++ test/datagen/insertUsers.sql
@@ -0,0 +1,32 @@
+INSERT INTO public."user"(login, name, password, email)
+	VALUES (
+        'buenaflor', 
+        'Giancarlo Buenaflor', 
+        '$argon2i$v=19$m=4096,t=3,p=1$6+hC9n7HJmYosek17OfH9w$BwRklOoIWTCdQ6ONS/berkAA4+yO92AIeQM2tdZzfEc', 
+        'gino1997@hotmail.de'
+    );
+
+INSERT INTO public."user"(login, name, password, email)
+	VALUES (
+        'buenaflor1', 
+        'Giancarlo Buenaflor1', 
+        '$argon2i$v=19$m=4096,t=3,p=1$6+hC9n7HJmYosek17OfH9w$BwRklOoIWTCdQ6ONS/berkAA4+yO92AIeQM2tdZzfEc', 
+        'gino1998@hotmail.de'
+    );
+
+INSERT INTO public."user"(login, name, password, email)
+	VALUES (
+        'buenaflor2', 
+        'Giancarlo Buenaflor2', 
+        '$argon2i$v=19$m=4096,t=3,p=1$6+hC9n7HJmYosek17OfH9w$BwRklOoIWTCdQ6ONS/berkAA4+yO92AIeQM2tdZzfEc', 
+        'gino1999@hotmail.de'
+    );
+
+INSERT INTO public."user"(login, name, password, email, bio)
+	VALUES (
+        'buenaflor3', 
+        'Giancarlo Buenaflor3', 
+        '$argon2i$v=19$m=4096,t=3,p=1$6+hC9n7HJmYosek17OfH9w$BwRklOoIWTCdQ6ONS/berkAA4+yO92AIeQM2tdZzfEc', 
+        'gino1997@hotmail.de',
+        'My Lorem Ipsum BIO'
+    );
