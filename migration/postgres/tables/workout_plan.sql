a17cf0437aad4f8f8a56abc2fc26eafe954f4d36
Giancarlo Buenaflor <e51837398@student.tuwien.ac.at>
2021-05-04 02:21:28 +0200

    Add workout_plan create table and datagen

---
+++ migration/postgres/tables/workout_plan.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS workout_plan (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    user_id uuid NOT NULL,
+    name text NOT NULL,
+    description text,
+    is_completed boolean NOT NULL DEFAULT false,
+    is_private boolean NOT NULL DEFAULT true,
+    start_date date,
+    end_date date,
+    email_confirmed boolean NOT NULL DEFAULT false,
+    completed_at timestamptz,
+    created_at timestamptz NOT NULL DEFAULT current_timestamp,
+    updated_at timestamptz NOT NULL DEFAULT current_timestamp,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES "user"(id),
+    CONSTRAINT name_length_check CHECK (length(name) <= 40),
+    CONSTRAINT bio_length_check CHECK (length(description) <= 500),
+    UNIQUE (name, user_id)
+);
